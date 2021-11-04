df96ad77af5914589d964d7ac63c4b994967607a
maniram chuahan <cr4m21@gmail.com>
2021-11-05 01:12:57 +0530

    conditionally run effects -- useEffect

--- src/components/ClassCounter.js
+++ src/components/ClassCounter.js
@@ -1,39 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class ClassCounter extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-             count:0
-        }
-    }
-    incrementCount = () =>{
-        this.setState(prevState =>{
-            return {
-                count : prevState.count+1
-            }
-        })
-    }
+    this.state = {
+      count: 0,
+      name: "",
+    };
+  }
+  incrementCount = () => {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  };
 
-    componentDidMount(){
-        document.title = `Clicked ${this.state.count} times`
+  componentDidMount() {
+    document.title = `Clicked ${this.state.count} times`;
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      document.title = `Clicked ${this.state.count} times`;
+      console.log(`updating document title`);
     }
-    componentDidUpdate(prevProps,prevState){
-        document.title = `Clicked ${this.state.count} times`
-    }
-    // componentDidUnount(){
-    //     document.title = `Clicked ${this.state.count} times`
-    // }
+  }
 
-    render() {
-        const {count} = this.state
-        return (
-            <div>
-            <button onClick={this.incrementCount}>click {count} times</button>
-            </div>
-        )
-    }
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <input
+          type="text"
+          name=""
+          id=""
+          value={this.state.name}
+          onChange={(e) => this.setState({ name: e.target.value })}
+        />
+        <button onClick={this.incrementCount}>click {count} times</button>
+      </div>
+    );
+  }
 }
 
-export default ClassCounter
+export default ClassCounter;
--- src/components/HookCounter1.js
+++ src/components/HookCounter1.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from "react";
 
 function HookCounter1() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState('')
   useEffect(() => {
     document.title = `Clicked ${count} times`;
-  });
+    console.log(`useEffect -> updating title name`);
+  },[count]);
   return (
     <div>
+    <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
       <button onClick={() => setCount(count + 1)}>Click {count} times</button>
     </div>
   );
