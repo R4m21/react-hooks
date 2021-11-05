1f2b5ce7ce1bb5948a51fd20b422b36b5ea91ced
maniram chuahan <cr4m21@gmail.com>
2021-11-05 15:26:49 +0530

    useContext Hook part 3 (its best way to context consume the props to avoid the nested call function using useContext() )

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { LastContext, UserContext } from './ComponentC'
 import ComponentF from './ComponentF'
 
 function ComponentE() {
+    const user = useContext(UserContext)
+    const last = useContext(LastContext)
     return (
         <div>
             <ComponentF/>
+            <div>
+               full name is : {user+" "+last}
+            </div>
         </div>
     )
 }
