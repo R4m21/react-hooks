078a4283880516e9b41bf484a6ca7a665766d454
maniram chuahan <cr4m21@gmail.com>
2021-11-05 18:33:17 +0530

    useReducer with useContext for Global state change counter value

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import './App.css';
+import React, { useReducer } from "react";
+import "./App.css";
 // import CounterR2 from './components/CounterR2';
-import CounterR3 from './components/CounterR3';
+// import CounterR3 from './components/CounterR3';
+import ComponentA from "./components/useReducer with useContext/ComponentA";
+import ComponentB from "./components/useReducer with useContext/ComponentB";
+import ComponentC from "./components/useReducer with useContext/ComponentC";
 // import ComponentC from './components/ComponentC';
 // import CounterR1 from './components/CounterR1';
 // import DataFetching3 from './components/DataFetching3';
@@ -18,27 +22,54 @@ import CounterR3 from './components/CounterR3';
 // import HookCounter from './components/HookCounter';
 // import HookCounterTwo from './components/HookCounterTwo';
 
+export const CountContext = React.createContext();
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
-    {/* <ClassCounter/> */}
-    {/* <HookCounter/> */}
-    {/* <HookCounterTwo/> */}
-    {/* <HookCounterThree/> */}
-    {/* <HookCounterFour/> */}
-    {/* <HookCounter1/> */}
-    {/* <ClassMouse/> */}
-    {/* <HookMouse/> */}
-    {/* <MouseContainer/> */}
-    {/* <IntervalClassCounter/>
+      {/* <ClassCounter/> */}
+      {/* <HookCounter/> */}
+      {/* <HookCounterTwo/> */}
+      {/* <HookCounterThree/> */}
+      {/* <HookCounterFour/> */}
+      {/* <HookCounter1/> */}
+      {/* <ClassMouse/> */}
+      {/* <HookMouse/> */}
+      {/* <MouseContainer/> */}
+      {/* <IntervalClassCounter/>
     <IntervalHookCounter/> */}
-    {/* <DataFetching/> */}
-    {/* <DataFetching2/> */}
-    {/* <DataFetching3/> */}
-    {/* <ComponentC/> */}
-    {/* <CounterR1/> */}
-    {/* <CounterR2/> */}
-    <CounterR3/>
+      {/* <DataFetching/> */}
+      {/* <DataFetching2/> */}
+      {/* <DataFetching3/> */}
+      {/* <ComponentC/> */}
+      {/* <CounterR1/> */}
+      {/* <CounterR2/> */}
+      {/* <CounterR3/> */}
+      <CountContext.Provider
+        value={{ countState: count, countDispatch: dispatch }}
+      >
+        <div>
+          Count : {count}
+          <ComponentA />
+          <ComponentB />
+          <ComponentC />
+        </div>
+      </CountContext.Provider>
     </div>
   );
 }
